687a4765654d202ded3c8cfc948643aff2a6651d
Sebastian Schöbinger <sebastian@lightbook.fritz.box>
2020-10-27 13:12:07 +0100

    feat: add encrypted credentials

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,21 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Imports the Google Cloud client library
+import { PubSub } from '@google-cloud/pubsub';
+import {getDecryptedSecret} from '../../api/utils/decret-secret'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+const projectId = 'iot-albert'
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub({projectId, credentials: getDecryptedSecret()});
+
+module.exports = async (req, res) => {
+    await quickstart()
+    const { name = 'World' } = req.query
+    res.status(200).send(`Hello ${name}!`)
 }
+
+async function quickstart(
+    topicName = 'projects/iot-albert/topics/node-red', // Name for the new topic to create
+  ) {
+  
+    const messageId = await pubSubClient.topic(topicName).publish(Buffer.from('yeah!'));
+    console.log(`Message ${messageId} published.`);
+  }
